9878fc7bf5440e73fd9b9b5801cdcd3eaf06f7b3
don-bigdad <bogdan.maksimenko.2002@gmail.com>
2022-08-06 23:02:45 +0300

    it remains to spread the templates

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import *
 # Create your views here.
+
 def response_base(request):
-    return HttpResponse("Hello from base page")
+    categories = Category.objects.all()
+    dish = Dish.objects.all()
+    events = Event.objects.all()
+    data ={"categories":categories,"dish":dish,"events":events}
+    return render(request,"index.html",context=data)
--- my_cafe/settings.py
+++ my_cafe/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,"static"),
+)
+MEDIA_URL_URL = 'media/'
+MEDIA_URL_ROOT = os.path.join(BASE_DIR,"media/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
