c694357524f0a75d3225ee1b36388f8720025db9
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2022-08-08 18:22:02 +0300

    Update settings.py

--- my_cafe/settings.py
+++ my_cafe/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_0zh-7!i=yaa14pvw+67)@4k^l-126gk@q5@p2us0uic8-yfom'
+# SECURITY WARNING: keep e secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
